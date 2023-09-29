b4ba74955a3c3a8206dcb6e5ee6a9c1ede9b36b5
Carlos Henrique de Souza Pinto <chspcontato@gmail.com>
2023-09-29 12:50:11 -0300

    Tentativas e mais tentativas de arrumar a rotação (falhei novamente)

--- Assets/Artes/Pokemons/Prefabs/LookRef.cs
+++ Assets/Artes/Pokemons/Prefabs/LookRef.cs
@@ -1,30 +1,40 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class LookRef : MonoBehaviour
 {
-    GameObject reference;
-    //[Range(0.01f, 10f)]
-    [SerializeField] float velocidadeRotacao = 1f; // Velocidade de rotação
+    private bool colidiu = false;
 
+    [Header("Ajuste para a posicao do Pokemon")]
+    [SerializeField] Vector3 save_pos;
 
+    // A velocidade de rotação no eixo Y
+    public float velocidadeRotacao = 50.0f;
 
-    // Start is called before the first frame update
     void Start()
     {
-        reference = GameObject.Find("Ref_poke");
+
     }
 
     void Update()
     {
-        if (reference != null)
+        if (colidiu == false)
         {
-            Vector3 direcao = transform.position - reference.transform.position;
+            // Girar apenas no eixo Y
+            transform.Rotate(save_pos.x, velocidadeRotacao * Time.deltaTime, save_pos.z);
+        }
+        else
+        {
+            Debug.Log("Estou falso!");
+        }
+    }
 
-            Quaternion rotacaoDesejada = Quaternion.LookRotation(direcao);
-
-            transform.rotation = Quaternion.Slerp(transform.rotation, rotacaoDesejada, Time.deltaTime * velocidadeRotacao);
+    void OnTriggerEnter(Collider collision)
+    {
+        if (collision.gameObject.CompareTag("Pokemon"))
+        {
+            // Parar de girar quando colidir com um objeto com a tag "Pokemon"
+            colidiu = true;
+            Debug.Log("Colidi com Pokemon inimigo!!");
         }
     }
 }
