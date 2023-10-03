d2615bfd04be12e54be099d484eddbbf52b64787
Carlos Henrique de Souza Pinto <chspcontato@gmail.com>
2023-10-03 14:54:49 -0300

    Att

--- Assets/Shaders/LevelChanger.cs
+++ Assets/Shaders/LevelChanger.cs
@@ -14,21 +14,42 @@ public class LevelChanger : MonoBehaviour
     [Space(15)]
     [SerializeField] Vector3 tpPoint;
 
+    [SerializeField] GameObject TransicaoGO;
+    [SerializeField] byte alpha;
+
+    
+
     void Start()
     {
         playerInZone = false;
         player = GameObject.Find("Player");
+        TransicaoGO = GameObject.FindWithTag("Transicao");
+        
 
     }
 
     void Update()
     {
-
         if (playerInZone)
         {
             player.transform.position = tpPoint;
+            StartCoroutine(Transicao());
         }
 
+    }
+
+    public IEnumerator Transicao()
+    {
+        TransicaoGO.SetActive(true);
+        var _imagem = TransicaoGO.GetComponent<Image>();
+        _imagem.color = new Color32(0, 0, 0, alpha);
+
+        if (alpha <= 0)
+        {
+            TransicaoGO.SetActive(false);
+            yield break;
+        }
+        else { alpha -= 1; _imagem.color = new Color32(0, 0, 0, alpha); }
     }
 
     private void OnTriggerEnter(Collider collision)
