9d7672452e4223b0ad7389600fc4f1aaa835a7b7
meioanao <131088458+meioanao@users.noreply.github.com>
2023-10-05 16:11:26 -0300

    escolha inicias pronto

--- Assets/QuestController.cs
+++ Assets/QuestController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.SceneManagement;
+using System.Security.Authentication.ExtendedProtection;
 
 public class QuestController : MonoBehaviour
 {
@@ -12,30 +13,39 @@ public class QuestController : MonoBehaviour
     [SerializeField] GameObject mesaCarvalho;
     [SerializeField] GameObject escolhaPokemon;
     [SerializeField] GameObject player;
-
     [SerializeField] PokemonParty pokemonParty;
 
 
+    [SerializeField] public List<Pokemon> pokeInicial;
+
+    bool npcRota1 = true;
+
     public void Start()
     {
-        pokemonParty = player.getComponent<PokemonParty>();
+        pokemonParty = player.GetComponent<PokemonParty>();
     }
+
     public void Squirtle()
     {
         Time.timeScale = 1f;
+        pokemonParty.pokemons.Insert(0, pokeInicial[0]);
         escolhaPokemon.SetActive(false);
-        
+        npcRota1 = false;
     }
 
     public void Bulbassauro()
     {
         Time.timeScale = 1f;
+        pokemonParty.pokemons.Insert(1, pokeInicial[1]);
         escolhaPokemon.SetActive(false);
+        npcRota1 = false;
     }
 
     public void Charmannder()
     {
         Time.timeScale = 1f;
+        pokemonParty.pokemons.Insert(2, pokeInicial[2]);
         escolhaPokemon.SetActive(false);
+        npcRota1 = false;
     }
 }
