14c1a0751ad815c068525ab69718010a9861deda
meioanao <matheusfaria04@gmail.com>
2023-11-09 14:55:16 -0300

    pc parte 1

--- Assets/GuardarPokemon.cs
+++ Assets/GuardarPokemon.cs
@@ -4,15 +4,20 @@ using UnityEngine;
 
 public class GuardarPokemon : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
 
-    // Update is called once per frame
-    void Update()
+    [SerializeField] SistemaDeBatalha sistema;
+    [SerializeField] PokemonParty playerParty;
+    [SerializeField] GameObject PcUI;
+
+    private void OnTriggerStay(Collider colisao)
     {
-        
+        if (colisao.gameObject.CompareTag("Player") && Input.GetKeyDown(KeyCode.Space))
+        {
+            PcUI.SetActive(true);
+            if( Input.GetKeyDown(KeyCode.Escape) ) 
+            {
+                PcUI.SetActive(false);
+            }
+        }
     }
 }
